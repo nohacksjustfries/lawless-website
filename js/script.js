9379ec41add85b3d643d321b97e99151411acea0
nohacksjustfries <arliascotney1205@gmail.com>
2026-07-26 14:11:28 +1000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("Lawless SMP Website Loaded");
+console.log("Lawless SMP Website");
+
+const copyButton = document.getElementById("copy-ip");
+
+if (copyButton) {
+
+    copyButton.addEventListener("click", function (event) {
+
+        event.preventDefault();
+
+        navigator.clipboard.writeText("lawlesssmp.net");
+
+        copyButton.textContent = "✓ Copied!";
+
+        setTimeout(() => {
+
+            copyButton.textContent = "Copy Server IP";
+
+        }, 2000);
+
+    });
+
+}
